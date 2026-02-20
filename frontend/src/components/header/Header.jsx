981b65a4e2ccc7aa2007bdce390fd2e86eec12d9
Guma <148246605+Gulmayram@users.noreply.github.com>
2026-02-20 23:31:07 +0600

    Update Header.jsx

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,43 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaSearch, FaFacebookF, FaInstagram, FaTwitter, FaSignInAlt, FaUserPlus, FaBars, FaTimes } from 'react-icons/fa';
-import logo from '../../assets/main_large1.png';
+import logo from '../../assets/main_large1.png'; 
 import './Header.css';
 
 const Header = ({ toggleMenu, isMenuOpen }) => {
   return (
-    <header className="header-main">
+    <header className="main-header">
       <div className="header-inner">
         
         <div className="header-left">
-          {/* Логотип появляется только на смартфонах (< 768px) */}
-          <div className="mobile-only-logo">
-            <img src={logo} alt="Кыргызгеология" />
-          </div>
+          {/* Логотип виден только на мобилках < 768px */}
+          <img src={logo} alt="Logo" className="mobile-logo-img" />
           
-          <div className="search-box">
+          <div className="search-wrapper">
             <FaSearch className="search-icon" />
-            <input type="text" placeholder="Поиск по сайту..." />
+            <input type="text" placeholder="Поиск по сайту..." className="search-input" />
           </div>
         </div>
 
         <div className="header-right">
-          <div className="social-links">
+          <div className="social-block">
             <a href="#"><FaFacebookF /></a>
             <a href="#"><FaInstagram /></a>
             <a href="#"><FaTwitter /></a>
           </div>
 
-          <div className="auth-group">
-            <button className="btn-auth">
+          <div className="auth-block">
+            <button className="auth-btn">
               <FaSignInAlt /> <span>Вход</span>
             </button>
-            <button className="btn-register">
+            <button className="reg-btn">
               <FaUserPlus /> <span>Регистрация</span>
             </button>
           </div>
 
-          {/* Бургер появляется только на смартфонах (< 768px) */}
-          <div className="burger-menu" onClick={toggleMenu}>
+          {/* Бургер строго < 768px */}
+          <div className="menu-burger" onClick={toggleMenu}>
             {isMenuOpen ? <FaTimes /> : <FaBars />}
           </div>
         </div>

c867aa364eb5c3174e3d48152d21bcc976f2479e
Guma <148246605+Gulmayram@users.noreply.github.com>
2026-02-20 23:15:06 +0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import Header from './components/Header/Header'; // Проверь этот путь!
-import Sidebar from './components/Sidebar/Sidebar';
-import Navbar from './components/Navbar/Navbar';
+import Header from './components/header/Header'; // Проверь этот путь!
+import Sidebar from './components/sidebar/Sidebar';
+import Navbar from './components/navbar/Navbar';
 import './App.css';
 
 function App() {
